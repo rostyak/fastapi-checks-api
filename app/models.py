0b00f8719d885e925a863b9a0bcf74a4e816ef85
r.nikulin <r.nikulin@busmarket.ua>
2025-05-29 16:00:25 +0300

    feat: Add models for receipt

--- app/models.py
+++ app/models.py
@@ -1,4 +1,7 @@
-from sqlalchemy import Column, Integer, String
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, DateTime
+from sqlalchemy.orm import relationship
 
 from .database import Base
 
@@ -10,3 +13,32 @@ class User(Base):
     name = Column(String, nullable=False)
     username = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
+    receipts = relationship("Receipt", back_populates="user")
+
+
+class Receipt(Base):
+    __tablename__ = "receipts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    payment_type = Column(String, nullable=False)
+    payment_amount = Column(Float, nullable=False)
+    total = Column(Float, nullable=False)
+    rest = Column(Float, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    user = relationship("User", back_populates="receipts")
+    items = relationship("ReceiptItem", back_populates="receipt")
+
+
+class ReceiptItem(Base):
+    __tablename__ = "receipt_items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    receipt_id = Column(Integer, ForeignKey("receipts.id"))
+    name = Column(String, nullable=False)
+    price = Column(Float, nullable=False)
+    quantity = Column(Float, nullable=False)
+    total = Column(Float, nullable=False)
+
+    receipt = relationship("Receipt", back_populates="items")
