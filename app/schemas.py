1ab1c8aacb9f0a9bf488d11e18e13e02d0fcb7f7
r.nikulin <r.nikulin@busmarket.ua>
2025-05-29 16:01:31 +0300

    feat: Add receipt schemas

--- app/schemas.py
+++ app/schemas.py
@@ -19,3 +19,41 @@ class UserLogin(BaseModel):
 class Token(BaseModel):
     access_token: str
     token_type: str = "bearer"
+
+
+class ProductInput(BaseModel):
+    name: str
+    price: float
+    quantity: float
+
+
+class PaymentType(str, Enum):
+    cashless = "cashless"
+    cash = "cash"
+
+
+class PaymentInput(BaseModel):
+    type: PaymentType
+    amount: float
+
+
+class ReceiptCreate(BaseModel):
+    products: List[ProductInput]
+    payment: PaymentInput
+
+
+class ProductOutput(ProductInput):
+    total: float
+
+
+class PaymentOutput(PaymentInput):
+    pass
+
+
+class ReceiptOutput(BaseModel):
+    id: int
+    products: List[ProductOutput]
+    payment: PaymentOutput
+    total: float
+    rest: float
+    created_at: datetime
