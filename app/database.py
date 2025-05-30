d0bd11f95c8007b14c1d59b843522887e346e16b
r.nikulin <r.nikulin@busmarket.ua>
2025-05-30 15:07:49 +0300

    refactor: Use env variables to create DB connection url

--- app/database.py
+++ app/database.py
@@ -1,8 +1,30 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./test.db"
+
+DATABASE_PREFIX = os.getenv("DATABASE_PREFIX", "postgresql")
+DB_USER = os.getenv("DB_USER", "")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_HOST = os.getenv("DB_HOST", "")
+DB_PORT = os.getenv("DB_PORT", 5432)
+DB_NAME = os.getenv("DB_NAME", "")
+
+if DB_USER and DB_PASSWORD and DB_HOST and DB_NAME:
+    DATABASE_URL = (
+        f"{DATABASE_PREFIX}://"
+        f"{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+elif DB_USER and DB_HOST and DB_NAME:
+    DATABASE_URL = (
+        f"{DATABASE_PREFIX}://"
+        f"{DB_USER}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+else:
+    DATABASE_URL = "sqlite:///./test.db"
+
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
